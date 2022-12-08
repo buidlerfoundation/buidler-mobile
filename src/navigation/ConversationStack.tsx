81adcee609af0427f2751d7159f3b217d53e6e45
HungNguyen <baohung2412@gmail.com>
2022-12-08 09:47:45 +0700

    Fix initial notification

--- src/navigation/ConversationStack.tsx
+++ src/navigation/ConversationStack.tsx
@@ -4,10 +4,12 @@ import PinPostScreen from 'screens/PinPostScreen';
 import ChannelDrawer from './ChannelDrawer';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SpaceDetailScreen from 'screens/SpaceDetailScreen';
+import {useRoute} from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
 const ConversationStack = () => {
+  const route = useRoute();
   const header = useCallback(() => null, []);
   const screenOptions = useCallback(
     () => ({
@@ -19,7 +21,11 @@ const ConversationStack = () => {
     <Stack.Navigator
       initialRouteName={DrawerID.ChannelDrawer}
       screenOptions={screenOptions}>
-      <Stack.Screen name={DrawerID.ChannelDrawer} component={ChannelDrawer} />
+      <Stack.Screen
+        name={DrawerID.ChannelDrawer}
+        component={ChannelDrawer}
+        initialParams={route.params}
+      />
       <Stack.Screen name={ScreenID.PinPostScreen} component={PinPostScreen} />
       <Stack.Screen
         name={ScreenID.SpaceDetailScreen}
