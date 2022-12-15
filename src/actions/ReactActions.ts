f3d723edc2c6db922872cba7522ec603d5ae9f15
HungNguyen <baohung2412@gmail.com>
2022-12-15 14:47:51 +0700

    add tap tic react

--- src/actions/ReactActions.ts
+++ src/actions/ReactActions.ts
@@ -1,10 +1,12 @@
 import {Dispatch} from 'redux';
 import api from 'services/api';
+import HapticUtils from 'utils/HapticUtils';
 
 export const addReact =
   (id: string, reactName: string, userId: string) =>
   async (dispatch: Dispatch) => {
     api.addReaction(id, {emoji_id: reactName, skin: 1});
+    HapticUtils.trigger();
     dispatch({
       type: 'ACTION_ADD_REACT',
       payload: {id, reactName, userId},
@@ -15,6 +17,7 @@ export const removeReact =
   (id: string, reactName: string, userId: string) =>
   async (dispatch: Dispatch) => {
     api.removeReaction(id, {emoji_id: reactName});
+    HapticUtils.trigger();
     dispatch({
       type: 'ACTION_REMOVE_REACT',
       payload: {id, reactName, userId},
