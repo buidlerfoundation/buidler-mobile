9af0a11ef59bad18222cdc0680d24a64cebccd15
HungNguyen <baohung2412@gmail.com>
2023-03-16 12:11:29 +0700

    Start working on scrollable wallet screen

--- src/screens/WalletScreen/index.tsx
+++ src/screens/WalletScreen/index.tsx
@@ -5,7 +5,6 @@ import useThemeColor from 'hook/useThemeColor';
 import AppStyles from 'common/AppStyles';
 import useWalletBalance from 'hook/useWalletBalance';
 import FastImage from 'react-native-fast-image';
-import {ScrollView, View} from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -14,52 +13,48 @@ const WalletScreen = () => {
   const walletBalance = useWalletBalance();
   if (!walletBalance?.coins || walletBalance?.coins?.length === 0) return null;
   return (
-    <ScrollView contentContainerStyle={{flex: 1}}>
-      <View style={{flex: 1}}>
-        <Tab.Navigator
-          screenOptions={{
-            tabBarScrollEnabled: true,
-            tabBarItemStyle: {
-              width: 'auto',
-              flexDirection: 'row',
-              alignItems: 'center',
-              paddingHorizontal: 0,
-              marginHorizontal: 5,
-            },
-            tabBarStyle: {
-              backgroundColor: colors.background,
-              marginTop: 40,
-              shadowOffset: {width: 0, height: 0},
-              elevation: 0,
-              marginHorizontal: 10,
-            },
-            tabBarLabelStyle: [
-              AppStyles.TextSemi15,
-              {textTransform: 'none', marginTop: 0, marginLeft: 0},
-            ],
-            tabBarInactiveTintColor: colors.subtext,
-            tabBarActiveTintColor: colors.text,
-            tabBarIndicator: () => null,
-          }}>
-          {walletBalance?.coins?.map(el => (
-            <Tab.Screen
-              key={el.contract.network}
-              name={el.contract.name}
-              component={WalletCollectibles}
-              initialParams={{token: el}}
-              options={{
-                tabBarIcon: () => (
-                  <FastImage
-                    style={{width: 20, height: 20}}
-                    source={{uri: el.contract.logo}}
-                  />
-                ),
-              }}
-            />
-          ))}
-        </Tab.Navigator>
-      </View>
-    </ScrollView>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarScrollEnabled: true,
+        tabBarItemStyle: {
+          width: 'auto',
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingHorizontal: 0,
+          marginHorizontal: 5,
+        },
+        tabBarStyle: {
+          backgroundColor: colors.background,
+          marginTop: 40,
+          shadowOffset: {width: 0, height: 0},
+          elevation: 0,
+          marginHorizontal: 10,
+        },
+        tabBarLabelStyle: [
+          AppStyles.TextSemi15,
+          {textTransform: 'none', marginTop: 0, marginLeft: 0},
+        ],
+        tabBarInactiveTintColor: colors.subtext,
+        tabBarActiveTintColor: colors.text,
+        tabBarIndicator: () => null,
+      }}>
+      {walletBalance?.coins?.map(el => (
+        <Tab.Screen
+          key={el.contract.network}
+          name={el.contract.name}
+          component={WalletCollectibles}
+          initialParams={{token: el}}
+          options={{
+            tabBarIcon: () => (
+              <FastImage
+                style={{width: 20, height: 20}}
+                source={{uri: el.contract.logo}}
+              />
+            ),
+          }}
+        />
+      ))}
+    </Tab.Navigator>
   );
 };
 
